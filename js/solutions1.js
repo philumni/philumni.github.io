56199c551941321a20a2cac7eb3b0fd9bbebe925
philumni <philumni@gmail.com>
2025-09-08 19:13:32 -0700

    Update solutions1.js, fixing warnings.

--- js/solutions1.js
+++ js/solutions1.js
@@ -8,18 +8,13 @@ myApp.isPalindrome = function(s)
 let leftIndex=0;
 let rightIndex=s.length-1;
 
-
-let lc=s.charAt(leftIndex);
-let rc=s.charAt(rightIndex);
-
 return (myApp.innerPalindrome(leftIndex,rightIndex,s));
 }
 
 
 myApp.innerPalindrome = function(leftIndex,rightIndex,s)
 {
-	console.log("testing");
-	if (leftIndex==rightIndex) return true;
+	if (leftIndex===rightIndex) return true;
 	if (leftIndex>rightIndex && rightIndex<leftIndex) return true;
 
 	if (s.charAt(leftIndex)!=s.charAt(rightIndex)) return false;
@@ -40,6 +35,5 @@ myApp.threeAnswer = function()
 	}
 	else answer.value="No, it's not a palindrome.";
 	console.log("tested " + word.value);
-return;
 }
 
